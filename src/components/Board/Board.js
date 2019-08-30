d9fd3dbea6909c71679579771680d0fd10baa69f
Agnieszka Koryl <ag.koryl@gmail.com>
2019-08-30 22:40:50 +0200

    hedgehog added

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -4,8 +4,8 @@ import './Board.css';
 import hedgehog from '../../assets/img/hedgehog.svg';
 import fox from '../../assets/img/fox.svg';
 import stump from '../../assets/img/stump.svg';
-import sink from '../../assets/img/sink.svg';
 import dice from '../../assets/img/dice.svg';
+import tunnel from '../../assets/img/tunnel.svg';
 
 class Board extends Component {
 
@@ -13,45 +13,67 @@ class Board extends Component {
         return (
             <div className="main-container">
                 <div className="game-board">
-                    <div className="game-field">
+                    <div className="hedgehog-container">
+                        <div className="hedgehog">
+                            <img src={hedgehog} alt="hedgehog"></img>
+                        </div>
                     </div>
                     <div className="game-field">
+                        1
                     </div>
                     <div className="game-field">
+                        2
                     </div>
                     <div className="game-field">
+                        3
                     </div>
                     <div className="game-field">
+                        4
                     </div>
                     <div className="game-field">
+                        5
                     </div>
                     <div className="game-field">
+                        6
                     </div>
                     <div className="game-field">
+                        7
                     </div>
                     <div className="game-field">
+                        8
                     </div>
                     <div className="game-field">
+                        9
                     </div>
                     <div className="game-field">
+                        10
+                    </div>
+                    <div className="game-field">
+                        11
                     </div>
                     <div className="game-field">
                         <img src={stump} alt="stump"></img>
                     </div>
                     <div className="game-field">
+                        13
                     </div>
                     <div className="game-field">
+                        14
                     </div>
                     <div className="game-field">
+                        15
                     </div>
                     <div className="game-field">
+                        16
                     </div>
                     <div className="game-field">
+                        17
                     </div>
                     <div className="game-field">
+                        18
                     </div>
                     <div className="game-field">
-
+                        <img src={tunnel} alt="tunnel"></img>
                     </div>
                     <div className="game-field">
                         <img src={fox} alt="fox"></img>
